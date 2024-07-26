732ce9157ef16751937e47f319a9a0f199840d09
MehdiGrimault <mehdi.grimault@gmail.com>
2024-07-26 16:22:23 +0200

    update count button component

--- front/src/components/molecules/CountButton/CountButton.tsx
+++ front/src/components/molecules/CountButton/CountButton.tsx
@@ -2,14 +2,23 @@ import "./CountButton.scss";
 import Icon from "../../atoms/Icon/Icon";
 
 interface CountButtonProps {
-    operation: 'plus' | 'minus';
-    onClick?: () => void;
+  operation: 'plus' | 'minus';
+  onClick?: (value: number) => void;
 }
 
-export default function CountButton({ operation, onClick }: CountButtonProps) {
+const CountButton: React.FC<CountButtonProps> = ({ operation, onClick }) => {
+  const handleClick = () => {
+      const value = operation === 'plus' ? 1 : -1;
+      if (onClick) {
+          onClick(value);
+      }
+  };
+
   return (
-    <div className="m-count-button" onClick={onClick}>
-        <Icon name={operation} color='white'/>
-    </div>
+      <div className="m-count-button" onClick={handleClick}>
+          <Icon name={operation} color="white" />
+      </div>
   );
-}
+};
+
+export default CountButton;
