19a50115e2c9c4ade7941c904fbe45008181e186
clementine <cleriv.72@gmail.com>
2024-11-08 15:48:29 +0100

    test register with correct config

--- backend/accounts/tests.py
+++ backend/accounts/tests.py
@@ -1,3 +1,25 @@
 from django.test import TestCase
+from django.apps import apps
+from accounts.models import User
+from accounts.views import RegisterView, LoginView
+from rest_framework.test import APIClient
+from rest_framework import status
+from django.urls import reverse
 
-# Create your tests here.
+User = apps.get_model('accounts', 'User')
+
+class RegisterTest(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+
+        self.register_url = reverse('register')
+
+    def test_user_can_register_with_corrects_email_and_password(self):
+        data= {
+            'email' : "test@test.com",
+            'password' : "password",
+            'confirmedPassword' : "password",
+        }
+        response = self.client.post(self.register_url, data, format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
--- backend/consumptions/tests.py
+++ backend/consumptions/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
+from consumptions.models import 
+    Motivation, 
+    Unit, 
+    Product, 
+    TrackedProduct, 
+    Consumption, 
+    TrackingFrequency,
 
-# Create your tests here.
